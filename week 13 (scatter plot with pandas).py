69c463e9e1cb74e066713f5738ee7334eaa70d6c
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-11-16 16:35:15 -0700

    Update week 13 (scatter plot with pandas).py

--- week 13 (scatter plot with pandas).py
+++ week 13 (scatter plot with pandas).py
@@ -6,13 +6,13 @@ import seaborn as sns
 mpg = pd.read_csv("mpg.csv")
     
 # Create a new data frame with the columns "weight" and "mpg"
-mpgSmall = mpg[[weight, mpg]]
+mpgSmall = mpg[["weight", "mpg"]]  # Fix the column names and use double brackets
 
 print(mpgSmall)
 
 # Create a scatter plot of weight vs mpg with x label "Weight" and y label "MPG"
-p = sns.scatterplot()
-p.set_xlabel()
-p.set_ylabel()
+p = sns.scatterplot(x="weight", y="mpg", data=mpgSmall)  # Specify x and y variables and the data
+p.set_xlabel("Weight")
+p.set_ylabel("MPG")
 
 plt.savefig('mpg_scatter.png')
